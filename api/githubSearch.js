bafd0b55c068abcef92569903790cbe6c2efdd9f
Arpad G. Bondor <arpad.g.bondor@gmail.com>
2022-03-13 09:52:38 +0000

    Test serverless

--- api/githubSearch.js
+++ api/githubSearch.js
@@ -1,3 +1,7 @@
-export default function handler(request, response) {
-    response.status(200).json('Hello world');
+import axios from 'axios';
+
+export default async function handler(request, response) {
+    const { query } = request.query;
+    const { data } = await axios.get('https://api.github.com/users/arpadgbondor');
+    response.status(200).json(`query: ${query}, method: ${request.method}, name: ${data.login}`);
 }
